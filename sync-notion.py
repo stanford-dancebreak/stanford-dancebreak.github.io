3ef8a030f03297d46bd455ee164f5cdf9c4f24b4
Evelyn Yee <yeevelyn@stanford.edu>
2025-11-10 13:30:39 -0800

    add notion-to-md-py dependency

--- sync-notion.py
+++ sync-notion.py
@@ -8,10 +8,10 @@ import yaml
 POSTS_DB = "264fe184d6788074a210c7cdfea69ed4"
 
 DJ_PAGE = "DJing-at-Dancebreak-DJ-Guidelines-266fe184d678806093b9da5cc7965a90"
-DJ_TITLE = "DJing at Dancebreak"
+DJ_TITLE = "DJing at Dancebreak - DJ Guidelines"
 
 INSTRUCTOR_PAGE = "Teaching-at-Dancebreak-Instructor-Guidelines-29dfe184d67880de843bd8bb70fd1101"
-INSTRUCTOR_TITLE = "Teaching at Dancebreak"
+INSTRUCTOR_TITLE = "Teaching at Dancebreak - Instructor Guidelines"
 
 HEADER_TEMPLATE = "---\ntitle\n---\n\n"
 
@@ -26,22 +26,24 @@ def main():
         f.write(output)
     
     # dj guidelines
+    dj_header = HEADER_TEMPLATE.replace("title", DJ_TITLE)
     dj_blocks = n2m.page_to_markdown(DJ_PAGE)
     # Convert markdown blocks to string
     dj_str = n2m.to_markdown_string(dj_blocks).get('parent')
+    dj_content = dj_str.split(DJ_TITLE)[-1].strip()
     # Write to a file
     with open("content/info/djing.md", "w") as f:
-        dj_header = HEADER_TEMPLATE.replace("title", DJ_TITLE)
-        f.write(dj_header + dj_str)
+        f.write(dj_header + dj_content)
 
     # instructor guidelines
+    inst_header = HEADER_TEMPLATE.replace("title", INSTRUCTOR_TITLE)
     inst_blocks = n2m.page_to_markdown(INSTRUCTOR_PAGE)
     # Convert markdown blocks to string
     inst_str = n2m.to_markdown_string(inst_blocks).get('parent')
+    inst_content = inst_str.split(INSTRUCTOR_TITLE)[-1].strip()
     # Write to a file
     with open("content/info/teaching.md", "w") as f:
-        inst_header = HEADER_TEMPLATE.replace("title", INSTRUCTOR_TITLE)
-        f.write(inst_header + inst_str)
+        f.write(inst_header + inst_content)
 
 
 if __name__ == "__main__":
